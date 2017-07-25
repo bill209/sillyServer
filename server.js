2aa04c6b496d7467b629a05a2f06813f0f085952
bill r <jy@ozzis.us>
2017-07-24 21:25:18 -0400

    adding initial db access

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
 var router     = express.Router();
+var dbInfo     = require('./db.js');
+
+const URI = 'mongodb://' + dbInfo.USER + ':' + dbInfo.PASS + '@sillydb-shard-00-00-cmpur.gcp.mongodb.net:27017,sillydb-shard-00-01-cmpur.gcp.mongodb.net:27017,sillydb-shard-00-02-cmpur.gcp.mongodb.net:27017/' + dbInfo.DB + '?ssl=true&replicaSet=sillydb-shard-0&authSource=admin'
+
+
+var MongoClient = require('mongodb').MongoClient;
+
+var uri = "mongodb://kay:myRealPassword@mycluster0-shard-00-00-wpeiv.mongodb.net:27017,mycluster0-shard-00-01-wpeiv.mongodb.net:27017,mycluster0-shard-00-02-wpeiv.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin";
+MongoClient.connect(URI, function(err, db) {
+	console.log('connected successfully to ' + dbInfo.DB);
+	db.close();
+});
+
 
 // bodyParser will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
