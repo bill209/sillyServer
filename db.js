9483b7c11dcc00d2b23a043d5ee4b741ce077de1
bill r <jy@ozzis.us>
2017-07-25 14:03:23 -0400

    debugging

--- db.js
+++ db.js
@@ -6,6 +6,7 @@ var dbInfo = {};
 
 if(process.env.MODE === 'prod'){
 	dbInfo.DB = process.env.DB;
+	dbInfo.DB = 'trains';  // temp requirement
 	dbInfo.PASS = process.env.PASS
 	dbInfo.USER = process.env.USER;
 	var URI = 'mongodb://' + dbInfo.USER + ':' + dbInfo.PASS + '@sillydb-shard-00-00-cmpur.gcp.mongodb.net:27017,sillydb-shard-00-01-cmpur.gcp.mongodb.net:27017,sillydb-shard-00-02-cmpur.gcp.mongodb.net:27017/' + dbInfo.DB + '?ssl=true&replicaSet=sillydb-shard-0&authSource=admin'
@@ -13,8 +14,6 @@ if(process.env.MODE === 'prod'){
 	dbInfo     = require('./dbInfo.js');
 	var URI = 'mongodb://localhost:27017/sillydb';
 }
-
-console.log('URI: ', URI);
 
 let DB = {
 
