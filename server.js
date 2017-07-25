74fcc6f9b11d04f650b70cc4ab367bb9c1f72d71
bill r <jy@ozzis.us>
2017-07-24 22:18:27 -0400

    testing

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ var MongoClient = require('mongodb').MongoClient;
 var uri = "mongodb://kay:myRealPassword@mycluster0-shard-00-00-wpeiv.mongodb.net:27017,mycluster0-shard-00-01-wpeiv.mongodb.net:27017,mycluster0-shard-00-02-wpeiv.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin";
 MongoClient.connect(URI, function(err, db) {
 	console.log('connected successfully to ' + dbInfo.DB);
+	console.log("db",db);
+
 	db.close();
 });
 
