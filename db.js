3bc80237325747a138e8cbfe023416ed61864b0c
bill r <jy@ozzis.us>
2017-07-25 13:33:01 -0400

    adding mongo calls

---
+++ db.js
@@ -0,0 +1,54 @@
+'use strict';
+
+let MongoClient = require('mongodb').MongoClient;
+
+var dbInfo = {};
+
+if(process.env.MODE === 'prod'){
+	dbInfo.DB = process.env.DB;
+	dbInfo.PASS = process.env.PASS
+	dbInfo.USER = process.env.USER;
+	const URI = 'mongodb://' + dbInfo.USER + ':' + dbInfo.PASS + '@sillydb-shard-00-00-cmpur.gcp.mongodb.net:27017,sillydb-shard-00-01-cmpur.gcp.mongodb.net:27017,sillydb-shard-00-02-cmpur.gcp.mongodb.net:27017/' + dbInfo.DB + '?ssl=true&replicaSet=sillydb-shard-0&authSource=admin'
+} else {
+	dbInfo     = require('./dbInfo.js');
+	var URI = 'mongodb://localhost:27017/sillydb';
+}
+
+
+let DB = {
+
+	connect: function(){
+		// connect to sillydb
+		return new Promise((resolve, reject) => {
+			MongoClient.connect(URI, (err, db) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(db);
+				}
+			});
+		});
+	},
+
+	close: function(db){
+		if(db){
+			db.close();
+		}
+	},
+
+	getTools: function(db){
+		return new Promise((resolve, reject) => {
+			var collection = db.collection('tools');
+			collection.find({}).toArray(function(err, res) {
+				if(err) {
+					reject(err);
+				} else {
+					resolve(res);
+				}
+			});
+		})
+	}
+
+}
+
+module.exports = DB;
--- server.js
+++ server.js
@@ -1,35 +1,33 @@
 // server.js
 // =============================================================================
-var express     = require('express');
-var app         = express();
-var bodyParser  = require('body-parser');
-var router      = express.Router();
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
+let DB = require('./db.js');
 
-var dbInfo = {};
-if(process.env.MODE === 'prod'){
-	dbInfo.DB = process.env.DB;
-	dbInfo.PASS = process.env.PASS
-	dbInfo.USER = process.env.USER;
-} else {
-	dbInfo     = require('./db.js');
-}
-const URI = 'mongodb://' + dbInfo.USER + ':' + dbInfo.PASS + '@sillydb-shard-00-00-cmpur.gcp.mongodb.net:27017,sillydb-shard-00-01-cmpur.gcp.mongodb.net:27017,sillydb-shard-00-02-cmpur.gcp.mongodb.net:27017/' + dbInfo.DB + '?ssl=true&replicaSet=sillydb-shard-0&authSource=admin'
+var dbc = {};
 
-// connect to sillydb
-MongoClient.connect(URI, function(err, db) {
-	db.close();
-});
+DB.connect()
+	.then(function (res) {
+			dbc = res;
+		},
+		function (err) {
+			console.log('connection error: ', err);
+			process.exit(1);
+		});
 
 
 // bodyParser will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
 //middleware
-app.use(function (req, res, next){
+app.use(function (req, res, next) {
+	console.log('something is happening...');
 	// redirect for hosted server
 	if (req.headers['x-forwarded-proto'] === 'https') {
 		res.redirect('http://' + req.hostname + req.url);
@@ -42,9 +40,24 @@ app.use(function (req, res, next){
 // =============================================================================
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });
+router.get('/', function (req, res) {
+	res.json({message: 'hooray! welcome to our api!'});
 });
+
+router.route('/tools')
+
+// get all the tools (accessed at GET http://localhost:8080/api/tools)
+	.get(function (req, res) {
+		return new Promise((resolve, reject) => {
+			DB.getTools(dbc)
+				.then(function (tools) {
+						resolve(res.json({tools: tools}));
+					},
+					function (err) {
+						reject(err)
+					});
+		});
+	});
 
 
 // register the routes -------------------------------
